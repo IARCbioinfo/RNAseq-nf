e18db58b41236baa0aa8da23f919f4d848a558d3
nalcala <nalcala@stanford.edu>
2017-06-15 17:33:52 +0200

    update docker and info for nextflow and circle ci

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ git add dag.html
 git commit -m "Generated DAG [skip ci]"
 git push origin $CIRCLE_BRANCH
 
-curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/alignment-nf/trigger/5360932c-62ae-4969-9581-312f4089660e/ 
+curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/nalcala/rnaseq-nf/trigger/a07f34d1-b3ce-4675-a1b2-3fee9362eae9/
 
 

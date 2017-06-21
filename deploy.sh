540f71ff4985933e4e9e51cbb39045cb948a32fc
nalcala <nalcala@stanford.edu>
2017-06-21 15:20:40 +0200

    add iarcbioinfo/rnaseq-nf information

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd ~/alignment-nf/
+cd ~/RNAseq-nf/
 git config --global user.email "alcalan@fellows.iarc.fr"
 git add dag.png
 git add dag.html
 git commit -m "Generated DAG [skip ci]"
 git push origin $CIRCLE_BRANCH
 
-curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/nalcala/rnaseq-nf/trigger/a07f34d1-b3ce-4675-a1b2-3fee9362eae9/
+curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/rnaseq-nf/trigger/a3a4905f-e04d-4a2c-8b8d-acf592b667e4/
 
 
